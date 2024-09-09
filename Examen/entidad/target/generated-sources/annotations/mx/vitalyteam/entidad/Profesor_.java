2cb46a50659eb6fb3ed888353b80cb8c6a0c60ee
GastiSoul99 <uziel.gutierrez@uabc.edu.mx>
2024-09-09 16:33:33 -0700

    v0.1.2 - Diseñe y cree la GUI de la captura de profesores
    Implementación del GUI para la captura de profesores, aun no las funciones.

--- Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/Profesor_.java
+++ Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/Profesor_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.vitalyteam.entidad.UnidadAprendizaje;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T15:24:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T16:18:41")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
