473b6d9cc547d4c7a9e06f29ab8eeb772d7c2765
Serenity <kronossky.7@gmail.com>
2024-09-09 21:57:37 -0700

    v0.1.4 Diseñe y cree la GUI del menu principal
    Elabore dos interfaces de menu principal, una para el usuario y otra para el administrador, considerando las diferencias en los servicios.

--- Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/Profesor_.java
+++ Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/Profesor_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.vitalyteam.entidad.UnidadAprendizaje;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T20:38:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T21:54:58")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
--- Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/UnidadAprendizaje_.java
+++ Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/UnidadAprendizaje_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.vitalyteam.entidad.Profesor;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T20:38:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T21:54:58")
 @StaticMetamodel(UnidadAprendizaje.class)
 public class UnidadAprendizaje_ { 
 
