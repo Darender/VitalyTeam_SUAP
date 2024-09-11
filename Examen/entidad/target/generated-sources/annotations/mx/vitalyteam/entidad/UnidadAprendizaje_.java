09156609331f67636de23d28b942fb6a9384f1a3
Serenity <kronossky.7@gmail.com>
2024-09-10 23:06:52 -0700

    v0.1.7
    Realice las pruebas correspondientes en las diferentes opciones del menu administrador y enlace las diferentes opciones.

--- Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/UnidadAprendizaje_.java
+++ Examen/entidad/target/generated-sources/annotations/mx/vitalyteam/entidad/UnidadAprendizaje_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.vitalyteam.entidad.Profesor;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T21:54:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-10T22:31:44")
 @StaticMetamodel(UnidadAprendizaje.class)
 public class UnidadAprendizaje_ { 
 
